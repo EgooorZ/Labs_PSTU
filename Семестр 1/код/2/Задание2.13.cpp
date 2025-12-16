b68db4f9471f07e04e23597e7fcf4a7ffb5b4101
Егор Завьялов <egorzavjalov040107@gmail.com>
2025-12-16 15:43:54 +0500

    Update Задание2.13.cpp

--- Семестр 1/код/2/Задание2.13.cpp
+++ Семестр 1/код/2/Задание2.13.cpp
@@ -1,15 +1,11 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
-#include <Windows.h>
 using namespace std;
 
 int main()
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
     int a, b, c, D;
     float x1, x2;
-    cout << "Введите коэффициенты уравнения: ";
     cin >> a;
     cin >> b;
     cin >> c;
@@ -19,16 +15,16 @@ int main()
     {
         x1 = ((-b) + sqrt(D)) / (2 * a);
         x2 = ((-b) - sqrt(D)) / (2 * a);
-        cout << "Корни уравнения: " << x1 << ", " << x2;
+        cout << "Roots equation: " << x1 << ", " << x2;
     }
     else if (D == 0)
     {
         x1 = (-b) / (2 * a);
-        cout << "Корень уравнения: " << x1;
+        cout << "Root equation: " << x1;
     }
     else
     {
-        cout << "Корней нет";
+        cout << "no roots";
     }
 
     return(0);
