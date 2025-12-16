7e59da9d39a6527f15631298ebc284357366d55f
Егор Завьялов <egorzavjalov040107@gmail.com>
2025-12-16 15:36:32 +0500

    Add files via upload

---
+++ Семестр 1/код/2/Задание2.12.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n, i, k, minn, maxx;
+    cin >> n;
+    if (n >= 1)
+    {
+        cin >> k;
+        minn = k;
+        maxx = k;
+        for (i = 0; i < n - 1; i++)
+        {
+            cin >> k;
+            if (k > maxx)
+            {
+                maxx = k;
+            }
+            if (k < minn)
+            {
+                minn = k;
+            }
+        }
+        cout << minn + maxx;
+    }
+    else
+    {
+        cout << "There is no sequence of this length";
+    }
+    return(0);
+}
+
